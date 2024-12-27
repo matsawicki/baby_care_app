e64f930dc7b78aa3a2eee2d5b29ac3f4ba78c008
mateusz <mateusz@geew.dev>
2024-12-27 23:30:55 +0100

    routers simplified

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from routers.enum import router as enum_router
+from routers import enum
 
 app = FastAPI()
 
-app.include_router(enum_router, prefix="/api/v1/enums", tags=["Enums"])
+app.include_router(enum.router)
 
 
